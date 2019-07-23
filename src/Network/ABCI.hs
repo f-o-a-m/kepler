a317006ae81b4d2cfb01989da8e39d4892d38e51
Martin Allen <blinky@foam.space>
2019-07-23 12:41:30 -0700

    remove dependency on network

--- src/Network/ABCI.hs
+++ src/Network/ABCI.hs
@@ -1,12 +1,10 @@
 module Network.ABCI where
 
-import           Control.Monad.IO.Class               (MonadIO (..))
-import           Control.Monad.Trans.Control          (MonadBaseControl)
 import           Data.Conduit                         (ConduitT, runConduit,
                                                        (.|))
 import qualified Data.Conduit.List                    as CL
 import           Data.Conduit.Network                 (AppData, ServerSettings,
-                                                       appSink, appSockAddr,
+                                                       appSink,
                                                        appSource,
                                                        runGeneralTCPServer,
                                                        serverSettings)
@@ -20,67 +18,60 @@ import           Network.ABCI.Internal.Wire           as Wire
 import           Network.ABCI.Types.App               (App (..))
 import qualified Network.ABCI.Types.Messages.Request  as Request
 import qualified Network.ABCI.Types.Messages.Response as Response
-import           Network.Socket                       (SockAddr)
 import qualified Proto.Types                          as PT
-import           UnliftIO                             (MonadUnliftIO)
-
 
 
 -- | Default ABCI app network settings.
-defaultSettings :: ServerSettings
-defaultSettings = serverSettings 26658 $ fromString "127.0.0.1"
+defaultLocalSettings :: ServerSettings
+defaultLocalSettings = serverSettings 26658 $ fromString "127.0.0.1"
 
 -- | Serve an ABCI application with custom 'ServerSettings'
 serveAppWith
-  :: ( MonadIO m
-     , MonadBaseControl IO m
-     , MonadUnliftIO m
-     )
+  :: Monad m
   => ServerSettings
-  -> (SockAddr -> m (App m))
-  -> m ()
-serveAppWith cfg mkApp = runGeneralTCPServer cfg $ \appData -> do
-  app <- mkApp (appSockAddr appData)
-  runConduit (setupConduit app appData)
+  -> (forall a. m a -> IO a)
+  -> App m
+  -> (AppData -> IO ())
+  -> IO ()
+serveAppWith cfg nat app onAquire = runGeneralTCPServer cfg $ \appData -> do
+  onAquire appData
+  runConduit $ setupConduit app nat appData
 
 -- | Serve an ABCI application with default 'ServerSettings'
 serveApp
-  :: ( MonadIO m
-     , MonadBaseControl IO m
-     , MonadUnliftIO m
-     )
-  => (SockAddr -> m (App m))
-  -> m ()
-serveApp = serveAppWith defaultSettings
+  :: Monad m
+  => (forall a. m a -> IO a)
+  -> App m
+  -> (AppData -> IO ())
+  -> IO ()
+serveApp = serveAppWith defaultLocalSettings
 
 -- | Sets up the application wire pipeline.
 setupConduit
-  :: ( MonadIO m
-     , MonadUnliftIO m
-     )
+  :: Monad m
   => App m
+  -> (forall a. m a -> IO a)
   -> AppData
-  -> ConduitT i o m ()
-setupConduit app appData =
+  -> ConduitT i o IO ()
+setupConduit app nat appData =
      appSource appData
   .| Wire.decodeLengthPrefixC
   .| CL.map (traverse PL.decodeMessage =<<)
-  .| CL.mapM (respondWith app)
+  .| CL.mapM (respondWith app nat)
   .| CL.map (map PL.encodeMessage)
   .| Wire.encodeLengthPrefixC
   .| appSink appData
 
 respondWith
-  :: ( Monad m
-     , MonadIO m
-     )
+  :: Monad m
   => App m
+  -> (forall a. m a -> IO a)
   -> Either String [PT.Request]
-  -> m [PT.Response]
-respondWith app eReq =
+  -> IO [PT.Response]
+respondWith app nat eReq =
   case eReq of
     Left err -> pure [makeResponseError ("Invalid request: " <> cs err)]
-    Right reqs -> forM reqs $ \req ->
+    Right reqs -> nat $ forM reqs $ \req ->
       Request.withProto req (runApp app)
   where
     runApp (App f) mParsedReq = case mParsedReq of
