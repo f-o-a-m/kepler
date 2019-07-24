f137f591b14fdbb3b34e8166ac9f587179c4da7d
blinky3713 <31280145+blinky3713@users.noreply.github.com>
2019-07-24 11:31:00 -0700

    Natural transformation (#24)
    * remove dependency on network
    
    * stylish, selective export
    
    * move natural transformation to special helper function
    
    * make serveApp simpler
    
    * generalize transformApp

--- src/Network/ABCI.hs
+++ src/Network/ABCI.hs
@@ -1,13 +1,14 @@
-module Network.ABCI where
+module Network.ABCI
+  ( defaultLocalSettings
+  , serveAppWith
+  , serveApp
+  ) where
 
-import           Control.Monad.IO.Class               (MonadIO (..))
-import           Control.Monad.Trans.Control          (MonadBaseControl)
 import           Data.Conduit                         (ConduitT, runConduit,
                                                        (.|))
 import qualified Data.Conduit.List                    as CL
 import           Data.Conduit.Network                 (AppData, ServerSettings,
-                                                       appSink, appSockAddr,
-                                                       appSource,
+                                                       appSink, appSource,
                                                        runGeneralTCPServer,
                                                        serverSettings)
 import           Data.Monoid                          ((<>))
@@ -20,47 +21,37 @@ import           Network.ABCI.Internal.Wire           as Wire
 import           Network.ABCI.Types.App               (App (..))
 import qualified Network.ABCI.Types.Messages.Request  as Request
 import qualified Network.ABCI.Types.Messages.Response as Response
-import           Network.Socket                       (SockAddr)
 import qualified Proto.Types                          as PT
-import           UnliftIO                             (MonadUnliftIO)
 
 
+-- | Default ABCI app network settings for serving on localhost at the
+-- standard port.
+defaultLocalSettings :: ServerSettings
+defaultLocalSettings = serverSettings 26658 $ fromString "127.0.0.1"
 
--- | Default ABCI app network settings.
-defaultSettings :: ServerSettings
-defaultSettings = serverSettings 26658 $ fromString "127.0.0.1"
-
--- | Serve an ABCI application with custom 'ServerSettings'
+-- | Serve an ABCI application with custom 'ServerSettings' and a custom
+-- action to perform on aquiring the socket resource.
 serveAppWith
-  :: ( MonadIO m
-     , MonadBaseControl IO m
-     , MonadUnliftIO m
-     )
-  => ServerSettings
-  -> (SockAddr -> m (App m))
-  -> m ()
-serveAppWith cfg mkApp = runGeneralTCPServer cfg $ \appData -> do
-  app <- mkApp (appSockAddr appData)
-  runConduit (setupConduit app appData)
+  :: ServerSettings
+  -> App IO
+  -> (AppData -> IO ())
+  -> IO ()
+serveAppWith cfg app onAquire = runGeneralTCPServer cfg $ \appData -> do
+  onAquire appData
+  runConduit $ setupConduit app appData
 
--- | Serve an ABCI application with default 'ServerSettings'
+-- | Serve an ABCI application with default local 'ServerSettings'
+-- and a no-op on aquiring the socket resource.
 serveApp
-  :: ( MonadIO m
-     , MonadBaseControl IO m
-     , MonadUnliftIO m
-     )
-  => (SockAddr -> m (App m))
-  -> m ()
-serveApp = serveAppWith defaultSettings
+  :: App IO
+  -> IO ()
+serveApp app = serveAppWith defaultLocalSettings app (const $ pure ())
 
 -- | Sets up the application wire pipeline.
 setupConduit
-  :: ( MonadIO m
-     , MonadUnliftIO m
-     )
-  => App m
+  :: App IO
   -> AppData
-  -> ConduitT i o m ()
+  -> ConduitT i o IO ()
 setupConduit app appData =
      appSource appData
   .| Wire.decodeLengthPrefixC
@@ -71,20 +62,17 @@ setupConduit app appData =
   .| appSink appData
 
 respondWith
-  :: ( Monad m
-     , MonadIO m
-     )
-  => App m
+  :: App IO
   -> Either String [PT.Request]
-  -> m [PT.Response]
+  -> IO [PT.Response]
 respondWith app eReq =
   case eReq of
     Left err -> pure [makeResponseError ("Invalid request: " <> cs err)]
     Right reqs -> forM reqs $ \req ->
-      Request.withProto req (runApp app)
+      Request.withProto req (run app)
   where
-    runApp (App f) mParsedReq = case mParsedReq of
+    run _app mParsedReq = case mParsedReq of
       Nothing        -> pure . makeResponseError $ "Invalid request"
-      Just parsedReq -> Response.toProto <$> f parsedReq
+      Just parsedReq -> Response.toProto <$> runApp _app parsedReq
     makeResponseError err =
       Response.toProto . Response.ResponseException  $ Response.Exception err
