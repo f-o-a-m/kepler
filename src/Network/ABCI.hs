1a38b0d793e56c1b53031e474a3802db6312708f
Irakli Safareli <i.safareli@gmail.com>
2019-07-24 21:17:08 +0400

    step 2

--- src/Network/ABCI.hs
+++ src/Network/ABCI.hs
@@ -1,15 +1,15 @@
 module Network.ABCI where
 
 import           Control.Lens                         ((^.))
-import           Control.Monad.IO.Class               (MonadIO (..))
-import           Control.Monad.Trans.Control          (MonadBaseControl)
-import           Data.Conduit                         (ConduitT, runConduit,
-                                                       (.|))
+import           Control.Monad.Trans.Class            (lift)
+import           Control.Monad.Trans.Except           (runExceptT, except)
+import qualified Data.ByteString                      as BS
+import           Data.Conduit                         (runConduit, (.|))
 import qualified Data.Conduit.List                    as CL
 import           Data.Conduit.Network                 (AppData, ServerSettings,
-                                                       appSink, appSockAddr,
+                                                       appSink,
                                                        appSource,
-                                                       runGeneralTCPServer,
+                                                       runTCPServer,
                                                        serverSettings)
 import qualified Data.ProtoLens                       as PL
 import           Data.String                          (fromString)
@@ -21,74 +21,52 @@ import           Network.ABCI.Types.App               (App (..))
 import qualified Network.ABCI.Types.Error             as Error
 import qualified Network.ABCI.Types.Messages.Request  as Request
 import qualified Network.ABCI.Types.Messages.Response as Response
-import           Network.Socket                       (SockAddr)
 import qualified Proto.Types                          as PT
 import qualified Proto.Types_Fields                   as PT
-import           UnliftIO                             (MonadUnliftIO)
-
 
 -- | Default ABCI app network settings.
 defaultSettings :: ServerSettings
 defaultSettings = serverSettings 26658 $ fromString "127.0.0.1"
 
+type AppRunner m = BS.ByteString -> m BS.ByteString
+
 -- | Serve an ABCI application with custom 'ServerSettings'
 serveAppWith
-  :: ( MonadIO m
-     , MonadBaseControl IO m
-     , MonadUnliftIO m
-     )
-  => ServerSettings
-  -> (SockAddr -> m (App m))
-  -> m ()
-serveAppWith cfg mkApp = runGeneralTCPServer cfg $ \appData -> do
-  app <- mkApp (appSockAddr appData)
-  runConduit (setupConduit app appData)
+  :: ServerSettings
+  -> (AppData -> IO (AppRunner IO))
+  -> IO ()
+serveAppWith cfg mkRunner = runTCPServer cfg $ \appData -> do
+  runner <- mkRunner appData
+  runConduit $
+      appSource appData
+    .| CL.mapM runner
+    .| appSink appData
 
 -- | Serve an ABCI application with default 'ServerSettings'
 serveApp
-  :: ( MonadIO m
-     , MonadBaseControl IO m
-     , MonadUnliftIO m
-     )
-  => (SockAddr -> m (App m))
-  -> m ()
+  :: (AppData -> IO (AppRunner IO))
+  -> IO ()
 serveApp = serveAppWith defaultSettings
 
 
-
-    -- | Sets up the application wire pipeline.
-setupConduit
-  :: ( MonadIO m
-     , MonadUnliftIO m
-     )
-  => App m
-  -> AppData
-  -> ConduitT i o m ()
-setupConduit app appData =
-     appSource appData
-  .| CL.map Wire.decodeLengthPrefix
-  .| CL.map (traverse decodeRequestValue =<<)
-  .| CL.mapM (respondWith app)
-  .| CL.map (map PL.encodeMessage)
-  .| CL.map Wire.encodeLengthPrefix
-  .| appSink appData
+runApp :: Monad m => App m -> AppRunner m
+runApp (App app) bsInput = do
+  responcesE <- runExceptT $ do
+    packets <- except $ Wire.decodeLengthPrefix bsInput
+    requests <- except $ traverse decodeRequest packets
+    lift $ forM requests $ Request.withProto $ fmap Response.toProto . app
+  pure $ Wire.encodeLengthPrefix $ map PL.encodeMessage $ fromEither errToResponses responcesE
   where
-    decodeRequestValue input = case PL.decodeMessage input of
-      Left parseError -> Left $ Error.CanNotDecodeRequest input parseError
+    errToResponses err =
+      [Response.toProto $ Response.ResponseException $ Response.Exception $ cs $ Error.print err]
+
+    decodeRequest packet = case PL.decodeMessage packet of
+      Left parseError -> Left $ Error.CanNotDecodeRequest packet parseError
       Right (request :: PT.Request) -> case request ^. PT.maybe'value of
-        Nothing -> Left $ Error.NoValueInRequest input (request ^. PL.unknownFields)
+        Nothing -> Left $ Error.NoValueInRequest packet (request ^. PL.unknownFields)
         Just value -> Right $ value
 
-respondWith
-  :: ( Monad m
-     , MonadIO m
-     )
-  => App m
-  -> Either Error.Error [PT.Request'Value]
-  -> m [PT.Response]
-respondWith (App f) eReq =
-  case eReq of
-    Left err ->
-      pure [Response.toProto $ Response.ResponseException $ Response.Exception $ cs $ Error.print err]
-    Right reqs ->
-      forM reqs $ Request.withProto $ fmap Response.toProto . f
+    -- https://hackage.haskell.org/package/from-sum-0.2.1.0/docs/Control-FromSum.html#v:fromEither
+    fromEither :: (e -> a) -> Either e a -> a
+    fromEither f (Left x) = f x
+    fromEither _ (Right x) = x
