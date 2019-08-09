d567cf109ec50bbb99fed64d31055a9a7f69e438
Martin Allen <blinky@foam.space>
2019-08-09 10:11:12 -0700

    added test directory

---
+++ hs-abci-example/test/SimpleStorage/Test/E2ESpec.hs
@@ -0,0 +1,9 @@
+module SimpleStorage.Test.E2ESpec where
+
+import           Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "SimpleStorage E2E" $ do
+    it "Can query the initial count" $
+      pending
