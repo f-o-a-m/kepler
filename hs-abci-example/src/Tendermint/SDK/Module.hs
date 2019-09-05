9e537cf28668f2ae98d601ff2aa204d6a6b32d7b
Martin Allen <blinky@foam.space>
2019-09-04 17:11:19 -0700

    module setup from halogen

--- hs-abci-example/src/Tendermint/SDK/Module.hs
+++ hs-abci-example/src/Tendermint/SDK/Module.hs
@@ -1,21 +1,65 @@
 module Tendermint.SDK.Module where
 
---import Tendermint.SDK.Store
---import Tendermint.SDK.Codec
---import Network.ABCI.Types.Messages.Request (BeginBlock, EndBlock)
---
---data TendermintF msg store m a =
---    State (store -> m a)
---  | Transaction msg a
---  | BeginBlocker BeginBlock a
---  | EndBlocker EndBlock a
---  | Lift (m a)
---
---instance Functor m => Functor (TendermintF msg store m) where
---    fmap f a = case a of
---      State g -> State (fmap f . g)
---      Transaction msg b -> Transaction msg (f b)
---      BeginBlocker bb b -> BeginBlocker bb (f b)
---      EndBlocker eb b -> EndBlocker eb (f b)
---      Lift b -> Lift (f <$> b)
+import Data.Functor (($>))
+import Data.Foldable (traverse_)
+import Control.Monad.Free (Free)
+import Data.Functor.Coyoneda (Coyoneda(..))
+import Unsafe.Coerce (unsafeCoerce)
 
+--import Tendermint.SDK.Store
+
+data TendermintF state action m a =
+    State (state -> m a)
+  | Lift (m a)
+
+instance Functor m => Functor (TendermintF state action m) where
+    fmap f a = case a of
+      State g -> State (fmap f . g)
+      Lift b -> Lift (f <$> b)
+
+newtype TendermintM state action m a = TendermintM (Free (TendermintF state action m) a)
+  deriving (Functor, Applicative, Monad)
+
+data TendermintQ query action input a
+  = Initialize a
+  | Action action a
+  | Receive input a
+  | Query (Coyoneda query a) (() -> a)
+  deriving (Functor)
+
+data EvalSpec state query action input m = EvalSpec
+    { handleAction :: action -> TendermintM state action m ()
+    , handleQuery :: forall a. query a -> TendermintM state action m (Maybe a)
+    , receive :: input -> Maybe action
+    , initialize :: Maybe action
+    }
+
+mkEval
+  :: forall state query action input m.
+     Functor m
+  => EvalSpec state query action input m
+  -> (forall a. TendermintQ query action input a -> TendermintM state action m a)
+mkEval EvalSpec{..} q = case q of
+  Initialize a ->
+    traverse_ handleAction initialize $> a
+  Receive i a ->
+    traverse_ handleAction (receive i) $> a
+  Action action a ->
+    handleAction action $> a
+  Query (Coyoneda g a) f ->  maybe (f ()) g <$> handleQuery a
+
+data ComponentSpec state query action input m = ComponentSpec 
+  { initialState :: input -> state
+  , eval :: forall a. TendermintQ query action input a -> TendermintM state action m a
+  }
+
+data Component query input m
+
+mkComponent :: ComponentSpec state query action input m -> Component query input m
+mkComponent = unsafeCoerce
+
+unComponent 
+  :: forall query input m a. 
+     (forall state action. ComponentSpec state query action input m -> a)
+  -> Component query input m -> a
+unComponent = unsafeCoerce
