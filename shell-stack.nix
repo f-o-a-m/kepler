638cc224fa5f81d1426c5430d60e928644bc1598
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-10-22 01:19:56 +0100

    Fix mac build

--- shell-stack.nix
+++ shell-stack.nix
@@ -1,5 +1,9 @@
 { ghc
-, nixpkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/19.03.tar.gz) {}
+, nixpkgs ? import (builtins.fetchTarball {
+    name = "nixpkgs-PR-71165";
+    url = https://github.com/nixos/nixpkgs/archive/e3b740516f05307b55bcdb7e46c5d55da5fe0443.tar.gz;
+    sha256 = "0ssl12nl3n5bp23yng3naji0yw012a12xb2sa8kwdp2ln0qf8mcy";
+  }) {}
 }:
 with nixpkgs;
 
