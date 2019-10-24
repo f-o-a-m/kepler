c179b1efda7b7f3d90c4495bd887e7d88381ff67
Martin Allen <blinky@foam.space>
2019-10-24 09:26:38 -0700

    auth tree interpreter

--- hs-abci-example/src/Tendermint/SDK/AuthTreeStore.hs
+++ hs-abci-example/src/Tendermint/SDK/AuthTreeStore.hs
@@ -8,8 +8,9 @@ import qualified Crypto.Data.Auth.Tree.Class      as AT
 import qualified Crypto.Data.Auth.Tree.Cryptonite as Cryptonite
 import qualified Crypto.Hash                      as Cryptonite
 import           Data.ByteArray                   (convert)
+import Data.ByteString (ByteString)
 import           Tendermint.SDK.Store
-
+import Polysemy
 --------------------------------------------------------------------------------
 --
 --------------------------------------------------------------------------------
@@ -22,19 +23,23 @@ instance AT.MerkleHash AuthTreeHash where
     concatHashes (AuthTreeHash a) (AuthTreeHash b) = AuthTreeHash $ Cryptonite.concatHashes a b
 
 
-mkAuthTreeStore :: MonadIO m => m (RawStore m)
-mkAuthTreeStore = liftIO $ do
-  treeV <- newTVarIO AT.empty
-  pure $ RawStore
-    { rawStorePut = \k v -> liftIO . atomically $ do
+interpretAuthTreeStore
+  :: Member (Embed IO) r
+  => Sem (RawStore ': r) a
+  -> Sem r a
+interpretAuthTreeStore m = do
+  treeV :: TVar (AT.Tree ByteString ByteString) <- liftIO $ newTVarIO AT.empty
+  interpret
+    (\case
+      RawStorePut k v -> liftIO . atomically $ do
         tree <- readTVar treeV
         writeTVar treeV $ AT.insert k v tree
-    , rawStoreGet = \_ k -> liftIO . atomically $ do
+      RawStoreGet _ k -> liftIO . atomically $ do
         tree <- readTVar treeV
         pure $ AT.lookup k tree
-    , rawStoreProve = \_ _ -> pure Nothing
-    , rawStoreRoot = liftIO . atomically $ do
+      RawStoreProve _ _ -> pure Nothing
+      RawStoreRoot -> liftIO . atomically $ do
         tree <- readTVar treeV
         let AuthTreeHash r = AT.merkleHash tree :: AuthTreeHash
         pure $ Root $ convert r
-    }
+    ) m
