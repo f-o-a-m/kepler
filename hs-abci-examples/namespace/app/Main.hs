27a1fd6be8fff52dad40e7ded75c0764467b413f
Miloj Alverson <Milojalverson@Milojs-MacBook-Pro.local>
2019-11-04 19:26:46 -0800

    scaffold application

--- hs-abci-examples/namespace/app/Main.hs
+++ hs-abci-examples/namespace/app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
+import           Control.Exception           (bracket)
+import qualified Katip                       as K
+import           Namespace.Application   (makeAppConfig)
+import           Namespace.Server        (makeAndServeApplication)
+import           System.IO                   (stdout)
+import           Tendermint.SDK.Logger.Katip (LogConfig (..), mkLogConfig)
+
 
 main :: IO ()
-main = someFunc
+main = do
+  logCfg <- mkLogConfig "dev" "namespace"
+  handleScribe <- K.mkHandleScribe K.ColorIfTerminal stdout (K.permitItem K.DebugS) K.V2
+  let mkLogEnv = K.registerScribe "stdout" handleScribe K.defaultScribeSettings (_logEnv logCfg)
+  bracket mkLogEnv K.closeScribes $ \le -> do
+    cfg <- makeAppConfig logCfg {_logEnv = le}
+    makeAndServeApplication cfg
---
+++ hs-abci-examples/namespace/src/Namespace/Server.hs
@@ -0,0 +1,44 @@
+module Namespace.Server (makeAndServeApplication) where
+
+import           Data.Foldable                                 (fold)
+import           Data.Monoid                                   (Endo (..))
+import           Data.Proxy
+import           Network.ABCI.Server                           (serveApp)
+import           Network.ABCI.Server.App                       (Middleware)
+import qualified Network.ABCI.Server.Middleware.RequestLogger  as ReqLogger
+import qualified Network.ABCI.Server.Middleware.ResponseLogger as ResLogger
+import           Namespace.Application                     (AppConfig,
+                                                                AppError,
+                                                                Handler,
+                                                                runHandler)
+import           Tendermint.SDK.Application                    (MakeApplication (..),
+                                                                createApplication)
+import           Tendermint.SDK.Router                         (QueryApplication,
+                                                                serve)
+
+makeAndServeApplication :: AppConfig -> IO ()
+makeAndServeApplication cfg = undefined
+--  let serveRoutes :: QueryApplication Handler
+--      serveRoutes = serve (Proxy :: Proxy SS.Api) SS.server
+--      makeApplication :: MakeApplication Handler AppError
+--      makeApplication = MakeApplication
+--        { transformer = runHandler cfg
+--        , appErrorP = Proxy
+--        , app = simpleStorageApp serveRoutes
+--        , initialize = [SS.initialize]
+--        }
+--  putStrLn "Starting ABCI application..."
+--  application <- createApplication makeApplication
+--  serveApp =<< hookInMiddleware application
+--  where
+--    mkMiddleware :: IO (Middleware IO)
+--    mkMiddleware = do
+--      reqLogger <- ReqLogger.mkLogStdoutDev
+--      resLogger <- ResLogger.mkLogStdoutDev
+--      pure . appEndo . fold $
+--        [ Endo reqLogger
+--        , Endo resLogger
+--        ]
+--    hookInMiddleware _app = do
+--      middleware <- mkMiddleware
+--      pure $ middleware _app
