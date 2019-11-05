2b0a582a371f2376ed90a92439db59dd36dbcac5
Carl Factora <cfactora93@gmail.com>
2019-11-05 15:51:32 -0500

    convert hexstring hash to base64

--- hs-tendermint-client/kv-test/KVStore/Test/KVSpec.hs
+++ hs-tendermint-client/kv-test/KVStore/Test/KVSpec.hs
@@ -69,14 +69,17 @@ spec = do
       foundName `shouldBe` "satoshi"
       foundName `shouldBe` foundNameWProof
       -- check with /tx endpoint (w+w/o proof)
-      let hash = RPC.resultBroadcastTxCommitHash $ broadcastResp
-          txReq = def { RPC.requestTxHash = Just hash }
-          txReqWP = RPC.RequestTx { RPC.requestTxHash = Just hash
+      let hash = RPC.resultBroadcastTxCommitHash $ broadcastResp -- this is a hexstring
+          -- this actually works
+          -- baseHash = either (const "") id . Base64.base64String . encodeUtf8 $ "V9g1+7oNv5Itii7aVpIsmyTndgkn8kWnaEpzbEdp24o="
+          baseHash = Base64.fromBytes (Hex.toBytes hash :: ByteString)
+          txReq = def { RPC.requestTxHash = Just baseHash }
+          txReqWP = RPC.RequestTx { RPC.requestTxHash = Just baseHash
                                   , RPC.requestTxProve = True
                                   }
       _ <- runRPC $ RPC.tx txReq
       _ <- runRPC $ RPC.tx txReqWP
-      -- @NOTE: this doesn't work
+      
       pure ()
 
 encodeTx :: String -> Base64String
