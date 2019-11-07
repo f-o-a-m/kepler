0bc5d8f2d44c62f37668faef649a629d3a3be3cc
Carl Factora <cfactora93@gmail.com>
2019-11-07 12:37:21 -0500

    Use general wrapped list type

--- hs-abci-types/src/Network/ABCI/Types/Messages/FieldTypes.hs
+++ hs-abci-types/src/Network/ABCI/Types/Messages/FieldTypes.hs
@@ -82,17 +82,15 @@ instance FromJSON WrappedWord64 where
   parseJSON (String t) = pure . WrappedWord64 . read . unpack $ t
   parseJSON a          = WrappedWord64 <$> parseJSON a
 
--- https://github.com/tendermint/tendermint/blob/v0.32.2/types/block.go#L819~
--- https://github.com/tendermint/tendermint/blob/v0.32.2/types/evidence.go#L278
-newtype WrappedEvidenceData = WrappedEvidenceData
-  { unwrapEvidenceData :: [Evidence]
-  } deriving (Eq, Show, Generic)
+newtype WrappedList a = WrappedList [a] deriving (Eq, Show)
 
-instance ToJSON WrappedEvidenceData where
-  toJSON (WrappedEvidenceData ls) = toJSONList ls
-instance FromJSON WrappedEvidenceData where
-  parseJSON = withObject "WrappedEvidenceData" $ \v -> WrappedEvidenceData
-    <$> v .:? "evidence" .!= []
+instance ToJSON a => ToJSON (WrappedList a) where
+  toJSON (WrappedList as) = toJSON as
+
+instance FromJSON a => FromJSON (WrappedList a) where
+  parseJSON as@(Array _) = WrappedList <$> parseJSON as
+  parseJSON Null = pure $ WrappedList []
+  parseJSON _ = fail "WrappedList must be Array or Null"
 
 -- measured in nanoseconds
 data Timestamp =
