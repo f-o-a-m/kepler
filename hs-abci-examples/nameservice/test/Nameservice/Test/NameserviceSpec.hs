560b410f4dfc77e80c7a2270fe2b7d403c0f15d1
Carl Factora <cfactora93@gmail.com>
2019-11-08 17:13:18 -0500

    Add tests for querying names and checking account balances

--- hs-abci-examples/nameservice/test/Nameservice/Test/NameserviceSpec.hs
+++ hs-abci-examples/nameservice/test/Nameservice/Test/NameserviceSpec.hs
@@ -5,7 +5,16 @@ import           Test.Hspec
 spec :: Spec
 spec = do
   describe "Nameservice Spec" $ do
+    it "Can query account balances" $ do
+      pending
+
     it "Can create a name" $ do
+      pending
+
+    it "Can query for a name that exists" $ do
+      pending
+
+    it "Can query for a name that doesn't exist" $ do
       pending
 
     it "Can set a name value" $ do
