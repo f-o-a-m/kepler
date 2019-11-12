c1fce241ac2a6ded5300a4a2de5a42df12531b8a
Carl Factora <cfactora93@gmail.com>
2019-11-12 14:50:04 -0500

    fix shadowing

--- hs-abci-examples/nameservice/src/Nameservice/Modules/Nameservice/Keeper.hs
+++ hs-abci-examples/nameservice/src/Nameservice/Modules/Nameservice/Keeper.hs
@@ -4,8 +4,7 @@ module Nameservice.Modules.Nameservice.Keeper where
 
 import           Nameservice.Modules.Nameservice.Messages
 import           Nameservice.Modules.Nameservice.Types
-import           Nameservice.Modules.Token                (Address, Amount,
-                                                           HasTokenEff, burn,
+import           Nameservice.Modules.Token                (HasTokenEff, burn,
                                                            mint, transfer)
 import           Polysemy                                 (Member, Members, Sem,
                                                            interpret, makeSem)
@@ -101,8 +100,8 @@ buyName
   => MsgBuyName
   -> Sem r ()
 -- ^ did it succeed
-buyName msg@MsgBuyName{..} = do
-  let name = msgBuyNameName
+buyName msg = do
+  let name = msgBuyNameName msg
   mWhois <- getWhois name
   case mWhois of
     -- The name is unclaimed, go ahead and debit the account
