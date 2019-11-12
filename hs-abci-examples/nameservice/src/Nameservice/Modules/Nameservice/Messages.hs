ed1600276e69a187e83a02c1de0d5bd55c876ec7
Carl Factora <cfactora93@gmail.com>
2019-11-12 16:26:16 -0500

    Use cs

--- hs-abci-examples/nameservice/src/Nameservice/Modules/Nameservice/Messages.hs
+++ hs-abci-examples/nameservice/src/Nameservice/Modules/Nameservice/Messages.hs
@@ -4,7 +4,6 @@ import           Control.Lens                          ((^.))
 import           Data.Either                           (Either)
 import           Data.String.Conversions               (cs)
 import           Data.Text                             (Text)
-import           Data.Text                             as T
 import           Nameservice.Modules.Nameservice.Types (Name (..))
 import           Nameservice.Modules.Token             (Address (..), Amount,
                                                         fromProtoAmountVal)
@@ -20,8 +19,8 @@ data MsgSetName =  MsgSetName
 -- TL;DR. ValidateBasic: https://cosmos.network/docs/tutorial/set-name.html#msg
 fromProtoMsgSetName :: M.SetName -> Either Text MsgSetName
 fromProtoMsgSetName msg = do
-  msgName <- nonEmpty "MsgSetNameName" . T.unpack $ msg ^. M.name
-  msgValue <- nonEmpty "MsgSetNameValue" . T.unpack $ msg ^. M.value
+  msgName <- nonEmpty "MsgSetNameName" . cs $ msg ^. M.name
+  msgValue <- nonEmpty "MsgSetNameValue" . cs $ msg ^. M.value
   msgOwner <- nonEmpty "MsgSetNameOwner" . cs $ msg ^. M.owner
   return MsgSetName { msgSetNameName = Name msgName
                     , msgSetNameValue = msgValue
@@ -35,7 +34,7 @@ data MsgDeleteName = MsgDeleteName
 
 fromProtoMsgDeleteName :: M.DeleteName -> Either Text MsgDeleteName
 fromProtoMsgDeleteName msg = do
-  msgName <- nonEmpty "MsgDeleteNameName" . T.unpack $ msg ^. M.name
+  msgName <- nonEmpty "MsgDeleteNameName" . cs $ msg ^. M.name
   msgOwner <- nonEmpty "MsgDeleteNameOwner" . cs $ msg ^. M.owner
   return MsgDeleteName { msgDeleteNameName = Name msgName
                        , msgDeleteNameOwner = Address msgOwner
@@ -50,8 +49,8 @@ data MsgBuyName = MsgBuyName
 
 fromProtoMsgBuyName :: M.BuyName -> Either Text MsgBuyName
 fromProtoMsgBuyName msg = do
-  msgName <- nonEmpty "MsgBuyNameName" . T.unpack $ msg ^. M.name
-  msgValue <- nonEmpty "MsgBuyNameValue" . T.unpack $ msg ^. M.value
+  msgName <- nonEmpty "MsgBuyNameName" . cs $ msg ^. M.name
+  msgValue <- nonEmpty "MsgBuyNameValue" . cs $ msg ^. M.value
   msgBuyer <- nonEmpty "MsgBuyNameBuyer" . cs $ msg ^. M.buyer
   msgBid <- nonNegativeAmount . fromProtoAmountVal $ msg ^. M.bid
   return MsgBuyName { msgBuyNameName = Name msgName
@@ -61,9 +60,9 @@ fromProtoMsgBuyName msg = do
                     }
 
 nonEmpty :: (Eq a, Monoid a) => String -> a -> Either Text a
-nonEmpty field x | x == mempty = Left . T.pack $ (show field ++ ": value cannot be empty")
+nonEmpty field x | x == mempty = Left . cs $ (show field ++ ": value cannot be empty")
                  | otherwise = Right x
 
 nonNegativeAmount :: (Ord a, Num a) => a -> Either Text a
 nonNegativeAmount x | x > 0     = Right x
-                    | otherwise = Left . T.pack $ "Bid cannot be negative"
+                    | otherwise = Left . cs @Text $ "Bid cannot be negative"
