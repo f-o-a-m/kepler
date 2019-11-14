88ba70396072177423e3b8c739bffdc7a1a90ddb
Carl Factora <cfactora93@gmail.com>
2019-11-14 14:04:36 -0500

    Complete test example

--- hs-abci-examples/nameservice/src/Nameservice/Protogen.hs
+++ hs-abci-examples/nameservice/src/Nameservice/Protogen.hs
@@ -1,25 +1,35 @@
-{-# LANGUAGE MagicHash            #-}
+{-# LANGUAGE MagicHash #-}
 
 -- @NOTE: ^ possibly the only language extension needed from tutorial
 -- Seems to be the only requirement for generating the .proto string
 
 module Nameservice.Protogen where
 
-import qualified Data.ByteString.Lazy     as BL
-import qualified Data.ByteArray.HexString as Hex
-import           Data.Text.Lazy           (Text) -- @NOTE: lazy text
-import           Data.Word                (Word64)
-import           GHC.Exts                 (Proxy#, proxy#)
+import           Data.Aeson.Casing               (snakeCase)
+import qualified Data.ByteArray.HexString        as Hex
+import qualified Data.ByteString.Lazy            as BL
+import           Data.Text.Lazy                  (Text)
+import           Data.Word                       (Word64)
+import           GHC.Exts                        (Proxy#, proxy#)
 import           GHC.Generics
-import           Proto3.Suite             (DotProtoDefinition, HasDefault (..),
-                                           Message, MessageField (..), Named,
-                                           Primitive (..), message,
-                                           packageFromDefs, toProtoFileDef, fromByteString)
-import           Proto3.Suite.DotProto    as DotProto
-import qualified Proto3.Wire.Decode       as Decode
-import qualified Proto3.Wire.Encode       as Encode
+import           Proto3.Suite                    (DotProtoDefinition,
+                                                  HasDefault (..), Message,
+                                                  MessageField (..), Named,
+                                                  Primitive (..),
+                                                  fromByteString, message,
+                                                  packageFromDefs)
+import           Proto3.Suite.DotProto           as DotProto
+import           Proto3.Suite.DotProto.Rendering (RenderingOptions,
+                                                  defRenderingOptions)
+import qualified Proto3.Wire.Decode              as Decode
+import qualified Proto3.Wire.Encode              as Encode
+import           Proto3.Wire.Types               (FieldNumber (..))
+import qualified Text.PrettyPrint                as PP
 
+--------------------------------------------------------------------------------
 -- field instance derivations
+--------------------------------------------------------------------------------
+
 newtype Address = Address Hex.HexString deriving (Eq, Show, Ord)
 instance Primitive Address where
   encodePrimitive n (Address hx) = Encode.byteString n (Hex.toBytes hx)
@@ -48,33 +58,36 @@ instance MessageField Amount
 
 -- messages
 data SetName = SetName
-  { setName  :: Name
-  , setValue :: Text
-  , setOwner :: Address
+  { setNameName  :: Name
+  , setNameValue :: Text
+  , setNameOwner :: Address
   } deriving (Show, Eq, Generic)
 
 instance Message SetName
 instance Named SetName
 
 data BuyName = BuyName
-  { buyName  :: Name
-  , buyValue :: Text
-  , buyBuyer :: Address
-  , buyBid   :: Amount
+  { buyNameName  :: Name
+  , buyNameValue :: Text
+  , buyNameBuyer :: Address
+  , buyNameBid   :: Amount
   } deriving (Show, Eq, Generic)
 
 instance Message BuyName
 instance Named BuyName
 
 data DeleteName = DeleteName
-  { deleteName  :: Name
-  , deleteOwner :: Address
+  { deleteNameName  :: Name
+  , deleteNameOwner :: Address
   } deriving (Show, Eq, Generic)
 
 instance Message DeleteName
 instance Named DeleteName
 
+--------------------------------------------------------------------------------
 -- scratch
+--------------------------------------------------------------------------------
+
 testSN :: SetName
 testSN = SetName (Name "satoshi") "cool cats" (Address "01")
 
@@ -84,6 +97,8 @@ testBN = BuyName (Name "satoshi") "cool cats" (Address "01") (Amount 999)
 testDN :: DeleteName
 testDN = DeleteName (Name "satoshi") (Address "01")
 
+-- @NOTE: for some reason, encoding results in a lazy bytestring
+-- while the provided decoder only accepts a strict bytestring
 fromLazyByteString :: Message a => BL.ByteString -> Either Decode.ParseError a
 fromLazyByteString = fromByteString . BL.toStrict
 
@@ -95,9 +110,23 @@ fromLazyByteString = fromByteString . BL.toStrict
 -- λ> fromLazyByteString $ Proto3.Suite.toLazyByteString testDN :: Either Decode.ParseError DeleteName
 -- Right (DeleteName {deleteName = Name "satoshi", deleteOwner = Address HexString "0x01"})
 
--- extension
+--------------------------------------------------------------------------------
+-- Requires magic hash extension
+--------------------------------------------------------------------------------
+
+stripPrefixName :: DotProtoIdentifier -> DotProtoIdentifier -> FieldNumber -> PP.Doc
+stripPrefixName (Single typeName) (Single fieldName) _ =
+  let prefixLen = length typeName
+      fieldName' = Single . snakeCase . drop prefixLen $ fieldName
+  in pPrint fieldName'
+-- @NOTE: we don't yet need for other identifiers
+stripPrefixName _ _ _ = undefined
+
+stripPrefixOptions :: RenderingOptions
+stripPrefixOptions = defRenderingOptions { roSelectorName = stripPrefixName }
+
 protoFile :: String
-protoFile = toProtoFileDef $ packageFromDefs "nameservice"
+protoFile = toProtoFile stripPrefixOptions $ packageFromDefs "nameservice"
   ([ message (proxy# :: Proxy# SetName)
    , message (proxy# :: Proxy# BuyName)
    , message (proxy# :: Proxy# DeleteName)
