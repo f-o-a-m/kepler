a235496bc793f9df622669381ecf47fe36dc9566
Martin Allen <31280145+blinky3713@users.noreply.github.com>
2019-11-20 09:04:50 -0800

    Added module level and application level routers (#112)

--- hs-abci-sdk/src/Tendermint/SDK/TxRouter.hs
+++ hs-abci-sdk/src/Tendermint/SDK/TxRouter.hs
@@ -1,23 +1,58 @@
-module Tendermint.SDK.TxRouter where
+{-# LANGUAGE UndecidableInstances #-}
+module Tendermint.SDK.TxRouter
+  ( router
+  , Router(..)
+  ) where
 
+import           Crypto.Hash                      (Digest)
+import           Crypto.Hash.Algorithms           (SHA256)
 import           Data.ByteString                  (ByteString)
 import           Data.Proxy
+import           Data.String.Conversions          (cs)
+import           GHC.TypeLits                     (KnownSymbol, symbolVal)
 import           Polysemy                         (Member, Sem)
 import           Polysemy.Error                   (Error, throw)
 import           Tendermint.SDK.Auth              (AuthError (..))
-import           Tendermint.SDK.Crypto            (Secp256k1)
-import           Tendermint.SDK.Types.Transaction (RawTransaction, Tx (..),
+import           Tendermint.SDK.Codec             (HasCodec (..))
+import           Tendermint.SDK.Crypto            (RecoverableSignatureSchema,
+                                                   SignatureSchema (..))
+import           Tendermint.SDK.Errors            (AppError, SDKError (..),
+                                                   throwSDKError)
+import           Tendermint.SDK.Module
+import           Tendermint.SDK.Types.Message     (Msg (..))
+import           Tendermint.SDK.Types.Transaction (RawTransaction (..),
+                                                   RoutedTx (..), Tx (..),
                                                    parseTx)
 
-type Handler r msg = Tx Secp256k1 msg -> Sem r ()
-
 router
-  :: forall r .
+  :: forall alg ms r .
      Member (Error AuthError) r
-  => Handler r ByteString
+  => RecoverableSignatureSchema alg
+  => Message alg ~ Digest SHA256
+  => Router ms r
+  => Proxy alg
+  -> Modules ms r
   -> RawTransaction
   -> Sem r ()
-router h rawTx =
-  case parseTx (Proxy @Secp256k1) rawTx of
+router (p  :: Proxy alg) ms rawTx =
+  case parseTx p rawTx of
     Left errMsg -> throw $ TransactionParseError errMsg
-    Right tx    -> h tx
+    Right tx    -> route ms tx
+
+class Router ms r where
+  route :: forall alg. Modules ms r -> Tx alg ByteString -> Sem r ()
+
+instance (Member (Error AppError) r) => Router '[] r where
+  route NilModules Tx{txRoute}  =
+    throwSDKError $ UnmatchedRoute txRoute
+
+instance (Member (Error AppError) r, Router ms r, HasCodec msg, KnownSymbol name) => Router (Module name msg api r ': ms) r where
+  route (ConsModule m rest) tx@Tx{..}
+    | symbolVal (Proxy :: Proxy name) == cs txRoute = do
+        msg <- case decode $ msgData txMsg of
+          Left err           -> throwSDKError $ ParseError err
+          Right (msg :: msg) -> return msg
+        let msg' = txMsg {msgData = msg}
+            tx' = RoutedTx $ tx {txMsg = msg'}
+        moduleRouter m tx'
+    | otherwise = route rest tx
