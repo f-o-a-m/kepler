189297f55422a38b0ca28943e958511c7ef9372f
Carl Factora <cfactora93@gmail.com>
2019-12-04 17:16:30 -0500

    add metrics to nameservice

--- hs-abci-examples/nameservice/src/Nameservice/Server.hs
+++ hs-abci-examples/nameservice/src/Nameservice/Server.hs
@@ -10,6 +10,7 @@ import           Network.ABCI.Server                           (serveApp)
 import           Network.ABCI.Server.App                       (Middleware)
 import qualified Network.ABCI.Server.Middleware.RequestLogger  as ReqLogger
 import qualified Network.ABCI.Server.Middleware.ResponseLogger as ResLogger
+import qualified Network.ABCI.Server.Middleware.MetricsLogger  as MetLogger
 import           Polysemy                                      (Sem)
 import           Tendermint.SDK.Application                    (MakeApplication (..),
                                                                 createApplication)
@@ -33,9 +34,11 @@ makeAndServeApplication cfg = do
     mkMiddleware = do
       reqLogger <- ReqLogger.mkLogStdoutDev
       resLogger <- ResLogger.mkLogStdoutDev
+      metLogger <- MetLogger.mkMetricsLogDatadog 10516 -- local port for log collection (TDP)
       pure . appEndo . fold $
         [ Endo reqLogger
         , Endo resLogger
+        , Endo metLogger
         ]
     hookInMiddleware _app = do
       middleware <- mkMiddleware
