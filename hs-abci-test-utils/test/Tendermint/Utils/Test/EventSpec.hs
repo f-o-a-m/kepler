f66e856f8e981ce8a24bd0dc37514a01bda16800
Carl Factora <cfactora93@gmail.com>
2019-12-12 16:01:28 -0500

    use directory module hierarchy

---
+++ hs-abci-test-utils/test/Tendermint/Utils/Test/EventSpec.hs
@@ -0,0 +1,31 @@
+module Tendermint.Utils.Test.EventSpec where
+
+import qualified Data.Aeson                    as A
+import           GHC.Generics                  (Generic)
+import           Tendermint.SDK.BaseApp.Events (FromEvent (..), ToEvent (..),
+                                                makeEvent)
+import           Test.Hspec
+
+spec :: Spec
+spec = describe "Event Tests" $ do
+    it "Can serialize and deserialize and event" $ do
+      let transferEv =  Transfer
+            { to = "me"
+            , from = "you"
+            , amount = 1
+            }
+      fromEvent (makeEvent transferEv) `shouldBe` Right transferEv
+
+data Transfer = Transfer
+  { to     :: String
+  , from   :: String
+  , amount :: Int
+  } deriving (Eq, Show, Generic)
+
+instance A.ToJSON Transfer
+
+instance ToEvent Transfer where
+    makeEventType _ = "transfer"
+
+instance A.FromJSON Transfer
+instance FromEvent Transfer
