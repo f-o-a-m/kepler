8c4c7ea6835966a41fa13e0c8fa780c9aab33806
Charles Crain <craincharles@gmail.com>
2019-12-16 16:56:20 -0800

    grpc calls complete

--- hs-iavl-client/src/Database/IAVL/RPCCall.hs
+++ hs-iavl-client/src/Database/IAVL/RPCCall.hs
@@ -6,9 +6,127 @@ import           Network.HTTP2.Client        (ClientIO, TooMuchConcurrency)
 import Data.ProtoLens.Message (defMessage)
 
 import qualified Proto.Iavl.Api        as Api
+import qualified Proto.Google.Protobuf.Empty as PT (Empty)
 
 
+--------------------------------------------------------------------------------
+-- | get
+--------------------------------------------------------------------------------
+get
+  :: GrpcClient
+  -> Api.GetRequest
+  -> ClientIO (Either TooMuchConcurrency (RawReply Api.GetResponse))
+get = rawUnary (RPC :: RPC Api.IAVLService "get")
+
+--------------------------------------------------------------------------------
+-- | getVersioned
+--------------------------------------------------------------------------------
+getVersioned
+  :: GrpcClient
+  -> Api.GetVersionedRequest
+  -> ClientIO (Either TooMuchConcurrency (RawReply Api.GetResponse))
+getVersioned = rawUnary (RPC :: RPC Api.IAVLService "getVersioned")
+
+--------------------------------------------------------------------------------
+-- | getVersionededWithProof
+--------------------------------------------------------------------------------
+getVersionedWithProof
+  :: GrpcClient
+  -> Api.GetVersionedRequest
+  -> ClientIO (Either TooMuchConcurrency (RawReply Api.GetWithProofResponse))
+getVersionedWithProof = rawUnary (RPC :: RPC Api.IAVLService "getVersionedWithProof")
+
+--------------------------------------------------------------------------------
+-- | set
+--------------------------------------------------------------------------------
+set
+  :: GrpcClient
+  -> Api.SetRequest
+  -> ClientIO (Either TooMuchConcurrency (RawReply Api.SetResponse))
+set = rawUnary (RPC :: RPC Api.IAVLService "set")
+
+--------------------------------------------------------------------------------
+-- | remove
+--------------------------------------------------------------------------------
+remove
+  :: GrpcClient
+  -> Api.RemoveRequest
+  -> ClientIO (Either TooMuchConcurrency (RawReply Api.RemoveResponse))
+remove = rawUnary (RPC :: RPC Api.IAVLService "remove")
+
+--------------------------------------------------------------------------------
+-- | saveVersion
+--------------------------------------------------------------------------------
+saveVersion
+  :: GrpcClient
+  -> ClientIO (Either TooMuchConcurrency (RawReply Api.SaveVersionResponse))
+saveVersion gc = rawUnary (RPC :: RPC Api.IAVLService "saveVersion") gc defMessage
+
+--------------------------------------------------------------------------------
+-- | deleteVersion
+--------------------------------------------------------------------------------
+deleteVersion
+  :: GrpcClient
+  -> Api.DeleteVersionRequest
+  -> ClientIO (Either TooMuchConcurrency (RawReply Api.DeleteVersionResponse))
+deleteVersion = rawUnary (RPC :: RPC Api.IAVLService "deleteVersion")
+
+--------------------------------------------------------------------------------
+-- | version
+--------------------------------------------------------------------------------
+version
+  :: GrpcClient
+  -> ClientIO (Either TooMuchConcurrency (RawReply Api.VersionResponse))
+version gc = rawUnary (RPC :: RPC Api.IAVLService "version") gc defMessage
+
+--------------------------------------------------------------------------------
+-- | hash
+--------------------------------------------------------------------------------
 hash
   :: GrpcClient
   -> ClientIO (Either TooMuchConcurrency (RawReply Api.HashResponse))
 hash gc = rawUnary (RPC :: RPC Api.IAVLService "hash") gc defMessage
+
+--------------------------------------------------------------------------------
+-- | versionExists
+--------------------------------------------------------------------------------
+versionExists
+  :: GrpcClient
+  -> Api.VersionExistsRequest
+  -> ClientIO (Either TooMuchConcurrency (RawReply Api.VersionExistsResponse))
+versionExists = rawUnary (RPC :: RPC Api.IAVLService "versionExists")
+
+--------------------------------------------------------------------------------
+-- | verify
+--------------------------------------------------------------------------------
+verify
+  :: GrpcClient
+  -> Api.VerifyRequest
+  -> ClientIO (Either TooMuchConcurrency (RawReply PT.Empty))
+verify = rawUnary (RPC :: RPC Api.IAVLService "verify")
+
+--------------------------------------------------------------------------------
+-- | verifyItem
+--------------------------------------------------------------------------------
+verifyItem
+  :: GrpcClient
+  -> Api.VerifyItemRequest
+  -> ClientIO (Either TooMuchConcurrency (RawReply PT.Empty))
+verifyItem = rawUnary (RPC :: RPC Api.IAVLService "verifyItem")
+
+--------------------------------------------------------------------------------
+-- | verifyAbsence
+--------------------------------------------------------------------------------
+verifyAbsence
+  :: GrpcClient
+  -> Api.VerifyAbsenceRequest
+  -> ClientIO (Either TooMuchConcurrency (RawReply PT.Empty))
+verifyAbsence = rawUnary (RPC :: RPC Api.IAVLService "verifyAbsence")
+
+--------------------------------------------------------------------------------
+-- | rollback
+--------------------------------------------------------------------------------
+rollback
+  :: GrpcClient
+  -> ClientIO (Either TooMuchConcurrency (RawReply PT.Empty))
+rollback gc = rawUnary (RPC :: RPC Api.IAVLService "rollback") gc defMessage
--- hs-iavl-client/test/Database/IAVL/RPCCallSpec.hs
+++ hs-iavl-client/test/Database/IAVL/RPCCallSpec.hs
@@ -1,29 +1,70 @@
 
 module Database.IAVL.RPCCallSpec where
 
-import           Control.Monad.IO.Class         ( liftIO )
 import           Network.GRPC.Client.Helpers    ( grpcClientConfigSimple
                                                 , GrpcClient
+                                                , GrpcClientConfig(..)
                                                 , setupGrpcClient
                                                 )
-import           Network.HTTP2.Client           ( runClientIO )
+import Control.Lens ((&), (.~), (^.))
+import Data.ProtoLens.Message (defMessage)
+import           Network.GRPC.Client    ( uncompressed, RawReply)
+import           Network.HTTP2.Client           ( runClientIO, ClientIO, TooMuchConcurrency )
 import           Test.Hspec
 import           Database.IAVL.RPCCall
+import qualified Proto.Iavl.Api_Fields        as Api
 
 spec :: Spec
 spec = beforeAll initGrpcClient $ do
   describe "IAVL RPC calls" $ do
-    it "should call `hash` RPC method" $ \gc -> do
-      Right (Right (Right (_, _, Right hashResp))) <- runClientIO $ hash gc
-      liftIO $ print hashResp
+    it "should call `hash` RPC method on empty Iavl store and get empty root hash" $ \gc -> do
+      res <- runGrpc $ hash gc
+      res ^. Api.rootHash `shouldBe` ""
+
+    it "should call `set` RPC method and get false as result since it does not already exist" $ \gc -> do
+      let setReq = defMessage & Api.key .~ "test-key"
+                              & Api.value .~ "test-value"
+      res <- runGrpc $ set gc setReq
+      res ^. Api.result `shouldBe` False
+
+    it "should call `get` RPC method and get the expected value" $ \gc -> do
+      let getReq = defMessage & Api.key .~ "test-key"
+      res <- runGrpc $ get gc getReq
+      res ^. Api.value `shouldBe` "test-value"
+
+    it "should call `saveVersion` RPC method and get the latest hash" $ \gc -> do
+      _ <- runGrpc $ saveVersion gc
+      res <- runGrpc $ hash gc
+      res ^. Api.rootHash `shouldBe` "\209)\148\US[p\231<tWo7\222\209\205UX\200D\159\147\159\229\182GG\148\170\169\150\ETBI"
+
+    it "should call `saveVersion` RPC method and get the latest hash" $ \gc -> do
+      _ <- runGrpc $ saveVersion gc
+      res <- runGrpc $ hash gc
+      res ^. Api.rootHash `shouldBe` "\209)\148\US[p\231<tWo7\222\209\205UX\200D\159\147\159\229\182GG\148\170\169\150\ETBI"
+
+    it "should call `deleteVersion` RPC method and get the latest hash as empty" $ \gc -> do
+      pending
+      let delVerReq = defMessage & Api.version .~ 1
+      delVerRes <- runGrpc $ deleteVersion gc delVerReq
+      delVerRes ^. Api.rootHash `shouldBe` "\209)\148\US[p\231<tWo7\222\209\205UX\200D\159\147\159\229\182GG\148\170\169\150\ETBI"
+      _ <- runGrpc $ saveVersion gc
+      res <- runGrpc $ hash gc
+      res ^. Api.rootHash `shouldBe` ""
 
 initGrpcClient :: IO GrpcClient
 initGrpcClient =
-  let grpcClient = grpcClientConfigSimple "localhost" 8090 True
-  in  runClientIO (setupGrpcClient grpcClient) >>= \case
+  let grpcClient = grpcClientConfigSimple "0.0.0.0" 8090 False
+  in  runClientIO (setupGrpcClient (grpcClient{_grpcClientConfigCompression=uncompressed})) >>= \case
         Right gc -> pure gc
         _        -> error "Error creating GrpcClient"
 
+runGrpc :: ClientIO (Either TooMuchConcurrency (RawReply a)) -> IO a
+runGrpc f = runClientIO f >>= \case
+  Right (Right (Right (_, _, Right res))) -> pure res
+  Right (Right (Right (_, _, Left err))) -> error ("Error running grpc call: " <> show err)
+  Right (Right (Left err)) -> error ("Error running grpc call: " <> show err)
+  Right (Left err) -> error ("Error running grpc call: " <> show err)
+  Left err -> error ("Error running grpc call: " <> show err)
 
 
 
