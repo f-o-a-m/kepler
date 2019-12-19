b29682efe1360302c61615fb0e108ac2106f4ac6
Carl Factora <cfactora93@gmail.com>
2019-12-19 13:56:04 -0500

    builds

--- hs-abci-examples/nameservice/app/Main.hs
+++ hs-abci-examples/nameservice/app/Main.hs
@@ -1,17 +1,12 @@
 module Main where
 
 import           Control.Exception                         (bracket)
-import           Data.Text                                 (pack)
-import qualified Database.V5.Bloodhound                    as BH
 import qualified Katip                                     as K
-import qualified Katip.Scribes.ElasticSearch               as ES
 import           Nameservice.Application                   (AppConfig (..),
                                                             addScribesToLogEnv,
                                                             makeAppConfig)
 import           Nameservice.Server                        (makeAndServeApplication)
-import qualified Network.HTTP.Client                       as Client
 import           System.Environment                        (lookupEnv)
-import           System.IO                                 (stdout)
 import qualified Tendermint.SDK.BaseApp                    as BaseApp
 import qualified Tendermint.SDK.BaseApp.Logger.Katip       as KL
 import           Tendermint.SDK.BaseApp.Metrics.Prometheus (PrometheusConfig (..),
@@ -20,19 +15,16 @@ import qualified Text.Read                                 as T
 
 main :: IO ()
 main = do
-  -- Env vars
   mPrometheusPort <- lookupEnv "STATS_PORT"
-  -- Cfgs
   metCfg <- mkPrometheusConfig
-  -- logCfg <- mkLogConfig "dev" "nameservice"
   -- Log Environment
+  cfg@AppConfig{..} <- makeAppConfig
+    metCfg { metricsPort = T.read <$> mPrometheusPort }
+  let BaseApp.Context{..} = baseAppContext
+      mkLogEnv = return . KL._logEnv $ contextLogConfig
   bracket mkLogEnv K.closeScribes $ \le -> do
-    leWithScribes <- addScribesToLogEnv le
-    cfg@AppConfig{..} <- makeAppConfig
-      metCfg { metricsPort = T.read <$> mPrometheusPort }
-      -- logCfg { _logEnv = leWithScribes }
-    let BaseApp.Context{..} = baseAppContext
-        logCfg = contextLogConfig { KL._logEnv = le }
+    leWithScribes <- addScribesToLogEnv cfg le
+    let logCfg = contextLogConfig { KL._logEnv = leWithScribes }
         newBAContext = baseAppContext { BaseApp.contextLogConfig = logCfg }
         newCfg = cfg { baseAppContext = newBAContext }
-    makeAndServeApplication cfg
+    makeAndServeApplication newCfg
--- hs-abci-examples/simple-storage/app/Main.hs
+++ hs-abci-examples/simple-storage/app/Main.hs
@@ -1,16 +1,23 @@
 module Main where
 
-import           Control.Exception         (bracket)
-import qualified Katip                     as K
-import           SimpleStorage.Application (makeAppConfig)
-import           SimpleStorage.Server      (makeAndServeApplication)
-import           System.IO                 (stdout)
-
+import           Control.Exception                   (bracket)
+import qualified Katip                               as K
+import           SimpleStorage.Application           (AppConfig (..),
+                                                      makeAppConfig)
+import           SimpleStorage.Server                (makeAndServeApplication)
+import           System.IO                           (stdout)
+import qualified Tendermint.SDK.BaseApp              as BaseApp
+import qualified Tendermint.SDK.BaseApp.Logger.Katip as KL
 
 main :: IO ()
 main = do
+  cfg@AppConfig{..} <- makeAppConfig -- logCfg {_logEnv = le}
   handleScribe <- K.mkHandleScribe K.ColorIfTerminal stdout (K.permitItem K.DebugS) K.V2
-  let mkLogEnv = K.registerScribe "stdout" handleScribe K.defaultScribeSettings (_logEnv logCfg)
+  let BaseApp.Context{..} = baseAppContext
+      mkLogEnv = K.registerScribe "stdout" handleScribe K.defaultScribeSettings
+        (KL._logEnv contextLogConfig)
   bracket mkLogEnv K.closeScribes $ \le -> do
-    cfg <- makeAppConfig logCfg {_logEnv = le}
-    makeAndServeApplication cfg
+    let newLogConfig = contextLogConfig { KL._logEnv = le }
+        newContext = baseAppContext { BaseApp.contextLogConfig = newLogConfig }
+        newCfg = cfg { baseAppContext = newContext }
+    makeAndServeApplication newCfg
