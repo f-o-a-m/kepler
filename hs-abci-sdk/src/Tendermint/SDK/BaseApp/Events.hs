72a3dbe59745ab94a577e6c22820d7fed7f094bb
martyall <martin@foam.space>
2020-02-12 12:30:24 -0800

    fromEvent class

--- hs-abci-sdk/src/Tendermint/SDK/BaseApp/Events.hs
+++ hs-abci-sdk/src/Tendermint/SDK/BaseApp/Events.hs
@@ -7,16 +7,17 @@ module Tendermint.SDK.BaseApp.Events
 import qualified Data.Aeson                             as A
 import qualified Data.ByteArray.Base64String            as Base64
 import qualified Data.ByteString                        as BS
-import           Data.String.Conversions                (cs, ConvertibleStrings(..))
-import           Data.Int                                    (Int64)
-import Data.Text (Text)
-import GHC.Generics
+import           Data.Int                               (Int64)
+import           Data.String.Conversions                (ConvertibleStrings (..),
+                                                         cs)
+import           Data.Text                              (Text)
+import           GHC.Generics
 import           Network.ABCI.Types.Messages.FieldTypes (Event (..),
                                                          KVPair (..))
 import           Polysemy                               (Member, Sem)
 import           Polysemy.Output                        (Output, output)
 import qualified Tendermint.SDK.BaseApp.Logger          as Log
-import Tendermint.SDK.Codec (HasCodec(..))
+import           Tendermint.SDK.Codec                   (HasCodec (..))
 
 data EventValue = EVBytes BS.ByteString
                 | EVInt Int64
@@ -25,7 +26,7 @@ data EventValue = EVBytes BS.ByteString
 
 instance ConvertibleStrings EventValue BS.ByteString where
   convertString (EVBytes bs) = cs bs
-  convertString (EVInt int) = cs $ show int
+  convertString (EVInt int)  = cs $ show int
   convertString (EVString t) = cs t
 
 
@@ -43,7 +44,7 @@ instance (GToNamedEventPrimatives f) => GToNamedEventPrimatives (C1 c f) where
   gtoNamedEventPrimatives = gtoNamedEventPrimatives . unM1
 
 instance (Selector s, HasCodec a) => GToNamedEventPrimatives (S1 s (K1 i a)) where
-  gtoNamedEventPrimatives m1@(M1 x) = 
+  gtoNamedEventPrimatives m1@(M1 x) =
     let name = cs $ selName m1
         val = encode $ unK1 x
     in [(name, val)]
@@ -52,48 +53,37 @@ instance (GToNamedEventPrimatives a, GToNamedEventPrimatives b) => GToNamedEvent
   gtoNamedEventPrimatives (a :*: b) = gtoNamedEventPrimatives a <> gtoNamedEventPrimatives b
 
 class GToEvent1 f where
-  gmakeEventType1 :: f p -> String
-  gmakeEventData1 :: f p -> [(BS.ByteString, BS.ByteString)]
+  gmakeEvent1 :: f p -> Event
 
 instance (GToNamedEventPrimatives f, Datatype d) => GToEvent1 (D1 d f) where
-  gmakeEventType1 = datatypeName
-  gmakeEventData1 (M1 x) = gtoNamedEventPrimatives x
+  gmakeEvent1 m1@(M1 x) = Event
+    { eventType = cs $ datatypeName m1
+    , eventAttributes = (\(k, v) -> KVPair (Base64.fromBytes k) (Base64.fromBytes v)) <$> gtoNamedEventPrimatives x
+    }
 
 -- | A class representing a type that can be emitted as an event in the
 -- | event logs for the deliverTx response.
 class ToEvent e where
-  makeEventType :: e -> String
-  makeEventData :: e -> [(BS.ByteString, BS.ByteString)]
+  toEvent :: e -> Event
 
-  default makeEventData :: (Generic e, GToEvent1 (Rep e)) => e -> [(BS.ByteString, BS.ByteString)]
-  makeEventData = gmakeEventData1 . from
-
-  default makeEventType :: (Generic e, GToEvent1 (Rep e)) => e -> String
-  makeEventType = gmakeEventType1 . from
-
-makeEvent
-  :: ToEvent e
-  => e
-  -> Event
-makeEvent (e :: e) = Event
-  { eventType = cs $ makeEventType e
-  , eventAttributes = (\(k, v) -> KVPair (Base64.fromBytes k) (Base64.fromBytes v)) <$> makeEventData e
-  }
+  default toEvent :: (Generic e, GToEvent1 (Rep e)) => e -> Event
+  toEvent = gmakeEvent1 . from
 
 emit
   :: ToEvent e
   => Member (Output Event) r
   => e
   -> Sem r ()
-emit e = output $ makeEvent e
+emit e = output $ toEvent e
 
 -- | Special event wrapper to add contextual event_type info
 newtype ContextEvent t = ContextEvent t
 instance (A.ToJSON a, ToEvent a) => A.ToJSON (ContextEvent a) where
   toJSON (ContextEvent a) =
-    A.object [ "event_type" A..= makeEventType a
-             , "event" A..= A.toJSON a
-             ]
+    let Event{eventType} = toEvent a
+    in  A.object [ "event_type" A..= eventType
+                 , "event" A..= A.toJSON a
+                 ]
 instance Log.Select a => Log.Select (ContextEvent a) where
   select v (ContextEvent a) = Log.select v a
 
@@ -104,4 +94,5 @@ logEvent
   => e
   -> Sem r ()
 logEvent event = Log.addContext (ContextEvent event) $
-  Log.log Log.Info (cs $ makeEventType event)
+  let Event{eventType} = toEvent event
+  in Log.log Log.Info eventType
